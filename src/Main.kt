686ea7d076ee4f4d8a4027324462428702de8c45
majidhawa <majidhawaah@gmail.com>
2024-03-25 08:28:07 +0300

    added number 4 question

--- src/Main.kt
+++ src/Main.kt
@@ -1,30 +1,36 @@
 fun main() {
-    sentence("Barnie bakes brown bagels and buns")
+
    numbers(arrayOf(1,2,3))
    println (volumeOfSphere(3.14159))
-    isPalindrome("kayak")
-
+ println   (isPalindrome("kayak"))
 }
 
-fun sentence(word:String){
-
-}
-fun numbers(array: Array<Int>){
 
 
+fun numbers(array: Array<Int>):Any{
+
+
+    return array.sum()
+    return array.count()
+    return array.average()
 
 }
 
 fun volumeOfSphere(pie:Double): Double {
 
-    var num1 =4/3
-    var radius = 7*7*7
-    var volume= num1*radius*pie
+    var num1 = 4 / 3
+    var radius = 7 * 7 * 7
+    var volume = num1 * radius * pie
     return volume
-
 }
-fun isPalindrome(word: String){
 
-    word.s
+fun isPalindrome( word:String):Boolean {
+    var newWord= word.reversed()
+
+    if  (newWord==word){
+        return true }
+        else {
+            return false
+        }
 
 }
