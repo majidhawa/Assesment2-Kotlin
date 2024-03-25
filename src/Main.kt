bd38d05a157d597bb69efd353fcfc0264379c630
majidhawa <majidhawaah@gmail.com>
2024-03-25 08:21:26 +0300

    added a  kotlin file

---
+++ src/Main.kt
@@ -0,0 +1,30 @@
+fun main() {
+    sentence("Barnie bakes brown bagels and buns")
+   numbers(arrayOf(1,2,3))
+   println (volumeOfSphere(3.14159))
+    isPalindrome("kayak")
+
+}
+
+fun sentence(word:String){
+
+}
+fun numbers(array: Array<Int>){
+
+
+
+}
+
+fun volumeOfSphere(pie:Double): Double {
+
+    var num1 =4/3
+    var radius = 7*7*7
+    var volume= num1*radius*pie
+    return volume
+
+}
+fun isPalindrome(word: String){
+
+    word.s
+
+}
